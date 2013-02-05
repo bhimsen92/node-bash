f27a633615e0a4b3eee6a9d3872faf61e123c0d2
Bhimsen Kulkarni <bhimsen.kulkarni@inmobi.com>
2013-02-05 12:02:40 +0530

    removed redundant files from repo

--- userdfs.js
+++
@@ -1,7 +0,0 @@
-// I really have no idea why i am creating this Class.
-function UserDefinedFunction( args, stmtList, _returnStmt ){
-    this.args = args;
-    this.stmtList = stmtList;
-    this._returnStmt = _returnStmt;
-}
-module.exports = UserDefinedFunction;
